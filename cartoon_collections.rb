9ac6d6a2d43733a29654c626bfb4b78a8e4cdaaa
Christopher Dajer <chrisdajer@gmail.com>
2021-02-04 06:20:25 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def greet_characters(array)
   #
   # Print a custom greeting for each element
   array.each do |greet_characters|
-    puts "Hello #{greet_characters}!"
+    puts "fuck #{greet_characters}!"
   end
 end
 
